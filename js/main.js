d9809627385e26cda3a495ce0f72aff3fda1976a
jianxu <jianxu@lewatek.com>
2016-12-26 10:07:32 +0800

    revaNow compelete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-	
+
 });
+	$(window).scroll(function() {
+	  	var scroll_top = $(document).scrollTop();
+	  	if (scroll_top >= 800) {
+	  		$("#to-top").fadeIn(300);
+	  	} else {
+	  		$("#to-top").fadeOut(300);
+	  	}
+	});
+    $("#to-top").on('click', function() {
+    	var speed = 200;
+        $('body,html').animate({ scrollTop: 0 }, speed);
+        return false;
+    });
