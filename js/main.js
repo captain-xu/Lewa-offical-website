900b82959161e17f2ebb52d2672224dc274c590f
jianxu <jianxu@lewatek.com>
2017-01-19 17:52:32 +0800

    aboutUs

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
 });
 	$(window).scroll(function() {
 	  	var scroll_top = $(document).scrollTop();
-	  	if (scroll_top >= 800) {
+	  	if (scroll_top >= 300) {
 	  		$("#to-top").fadeIn(300);
+	  		$(".header").addClass('active');
 	  	} else {
 	  		$("#to-top").fadeOut(300);
+	  		$(".header").removeClass('active');
 	  	}
 	});
     $("#to-top").on('click', function() {
