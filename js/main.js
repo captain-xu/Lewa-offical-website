c26e24fffebd0c81b94cd7486f910b2e5559ae23
jianxu <jianxu@lewatek.com>
2017-02-17 11:21:51 +0800

    优化scroll监听

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-$(document).ready(function() {
 
+$(window).on("scroll", function() {
+  	var scroll_top = $(document).scrollTop();
+  	if (scroll_top >= 300) {
+  		$("#to-top").fadeIn(300);
+  		$(".header").addClass('active');
+  	} else {
+  		$("#to-top").fadeOut(300);
+  		$(".header").removeClass('active');
+  	}
 });
-	$(window).scroll(function() {
-	  	var scroll_top = $(document).scrollTop();
-	  	if (scroll_top >= 300) {
-	  		$("#to-top").fadeIn(300);
-	  		$(".header").addClass('active');
-	  	} else {
-	  		$("#to-top").fadeOut(300);
-	  		$(".header").removeClass('active');
-	  	}
-	});
-    $("#to-top").on('click', function() {
-        $('body,html').animate({ scrollTop: 0 }, 200);
-        return false;
-    });
+$("#to-top").on('click', function() {
+    $('body,html').animate({ scrollTop: 0 }, 200);
+    return false;
+});
