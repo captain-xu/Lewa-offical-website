1c40791c639394069348fbf7db7c28a62483ef63
jianxu <jianxu@lewatek.com>
2017-02-23 10:18:51 +0800

    优化滚动监听，防抖动

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
 
-$(window).on("scroll", function() {
-  	var scroll_top = $(document).scrollTop();
-  	if (scroll_top >= 300) {
-  		$("#to-top").fadeIn(300);
-  		$(".header").addClass('active');
-  	} else {
-  		$("#to-top").fadeOut(300);
-  		$(".header").removeClass('active');
-  	}
-});
+//防抖动
+function debounce(fn) {
+  var timer = null;
+  return function() {
+    var context = this;
+    var args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function() {
+      fn.apply(context, args);
+    }, 200);
+  }
+};
+
+function windowScroll() {
+    var scroll_top = $(document).scrollTop();
+    if (scroll_top >= 300) {
+      $("#to-top").fadeIn(300);
+      $(".header").addClass('active');
+    } else {
+      $("#to-top").fadeOut(300);
+      $(".header").removeClass('active');
+    }
+};
+
+$(window).on("scroll", debounce(windowScroll));
+
 $("#to-top").on('click', function() {
     $('body,html').animate({ scrollTop: 0 }, 200);
     return false;
