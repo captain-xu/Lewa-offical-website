728118608e542e02f8d6ee47f6b46840a8fd518c
jianxu <jianxu@lewatek.com>
2017-03-03 16:02:11 +0800

    animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 
+$(document).ready(function() {
+    $('body').on('click', function(event) {
+      event.stopPropagation();
+      var target = $(event.target);
+      if ( $(window).width() <= 760) {
+        if(target.closest("#menu").length === 0){
+          $(".header-nav").removeClass('active');
+        }
+      }
+    });
+
+  $("#menu").on('click', function() {
+    $(this).next().toggleClass('active');
+  });
+
+  $(window).on("scroll", debounce(windowScroll));
+
+  $("#to-top").on('click', function() {
+      $('body,html').animate({ scrollTop: 0 }, 200);
+      return false;
+  });
+});
+
 //防抖动
 function debounce(fn) {
   var timer = null;
@@ -22,10 +45,3 @@ function windowScroll() {
       $(".header").removeClass('active');
     }
 };
-
-$(window).on("scroll", debounce(windowScroll));
-
-$("#to-top").on('click', function() {
-    $('body,html').animate({ scrollTop: 0 }, 200);
-    return false;
-});
